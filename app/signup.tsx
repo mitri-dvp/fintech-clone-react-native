a3cd2292d7cf9bf455f53bf44e96f69142d7ce5b
mitri-dvp <mitri.dvp@gmail.com>
2024-07-01 16:44:10 -0500

    Signup page development
    Use of KeyboardAvoidingView to properly handle device keyboard and adjust the screen view
    Use of TextInput to handle user input
    Using the pattern Link asChild with TouchableOpacity to handle text links
    Use of <View> component with flex: 1 to push Sign up button to the bottom

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,12 +1,94 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import React, { useState } from "react";
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
+import { Link } from "expo-router";
 
 const Signup = () => {
+  const [countryCode, setCountryCode] = useState("+57");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const onSignup = async () => {};
+
   return (
-    <View>
-      <Text>Signup</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior="padding"
+      keyboardVerticalOffset={Platform.OS === "ios" ? 80 : 0}
+    >
+      <View style={defaultStyles.container}>
+        <Text style={defaultStyles.header}>Let's get started!</Text>
+        <Text style={defaultStyles.descriptionText}>
+          Enter your phone number. We will send you a confirmation code there
+        </Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Country code"
+            keyboardType="numeric"
+            value={countryCode}
+          />
+          <TextInput
+            style={[styles.input, { flex: 1 }]}
+            placeholder="Mobile number"
+            placeholderTextColor={Colors.gray}
+            keyboardType="numeric"
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+          />
+        </View>
+
+        <Link href={"/login"} replace asChild>
+          <TouchableOpacity>
+            <Text style={defaultStyles.textLink}>
+              Already have an account? Log in
+            </Text>
+          </TouchableOpacity>
+        </Link>
+
+        <View style={{ flex: 1 }} />
+
+        <TouchableOpacity
+          style={[
+            phoneNumber ? styles.input_enabled : styles.input_disabled,
+            defaultStyles.pillButton,
+            { marginBottom: 20 },
+          ]}
+          onPress={onSignup}
+        >
+          <Text style={defaultStyles.buttonText}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
 export default Signup;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 40,
+    flexDirection: "row",
+  },
+  input: {
+    backgroundColor: Colors.lightGray,
+    padding: 20,
+    borderRadius: 16,
+    fontSize: 20,
+    marginRight: 10,
+  },
+  input_disabled: {
+    backgroundColor: Colors.primaryMuted,
+  },
+  input_enabled: {
+    backgroundColor: Colors.primary,
+  },
+});
