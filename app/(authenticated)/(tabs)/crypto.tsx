835b42ed0ca93a581a9cb36d4ec6186414bd3ce0
mitri-dvp <mitri.dvp@gmail.com>
2024-07-03 15:43:02 -0500

    Enabled Expo API routes feature
    Added info and listing endpoints to interact with coinmarketcap API

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -1,7 +1,17 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 
 const Page = () => {
+  useEffect(() => {
+    const getCryptoListings = async () => {
+      const res = await fetch("/api/listings");
+      const data = await res.json();
+      console.log({ data });
+    };
+
+    getCryptoListings();
+  }, []);
+
   return (
     <View>
       <Text>Page</Text>
